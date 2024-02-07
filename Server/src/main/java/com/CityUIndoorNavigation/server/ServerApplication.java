1bf0191da4b3b340f5e2d894a33e82178087a605
Leonard12138 <lcpsken@gmail.com>
2024-02-08 00:49:34 +0800

    wifi upload

--- Server/src/main/java/com/CityUIndoorNavigation/server/ServerApplication.java
+++ Server/src/main/java/com/CityUIndoorNavigation/server/ServerApplication.java
@@ -3,11 +3,17 @@ package com.CityUIndoorNavigation.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.CityUIndoorNavigation.server.controller.LocateDataPickController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		log.info("Server started");
 	}
 
 }
