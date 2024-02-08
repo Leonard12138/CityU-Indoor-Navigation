0aef55fff9b42c79bb66aa09da775b50c59fe21a
Leonard12138 <75907531+Leonard12138@users.noreply.github.com>
2024-02-08 15:32:42 +0800

    database

--- Server/src/main/java/com/CityUIndoorNavigation/server/data/WifiData.java
+++ Server/src/main/java/com/CityUIndoorNavigation/server/data/WifiData.java
@@ -1,14 +1,25 @@
 package com.CityUIndoorNavigation.server.data;
 
 import lombok.Data;
+
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 @Data
 @Accessors(chain = true)
 @NoArgsConstructor
 public class WifiData {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id = 0;
+	
     private String imei;
     private float X = 0;
     private float Y = 0;
---
+++ Server/src/main/java/com/CityUIndoorNavigation/server/repository/WifiDataRepository.java
@@ -0,0 +1,38 @@
+package com.CityUIndoorNavigation.server.repository;
+
+import com.CityUIndoorNavigation.server.data.WifiData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface WifiDataRepository extends JpaRepository<WifiData, Integer> {
+	
+    // Insert (Save) method
+    <S extends WifiData> S save(S wifiData);
+
+    // Update method
+    <S extends WifiData> S saveAndFlush(S wifiData);
+
+    // Delete method
+    void deleteById(int id);
+
+    List<WifiData> findBySsid(String ssid);
+
+    List<WifiData> findByBssid(String bssid);
+
+    List<WifiData> findByImeiAndSsid(String imei, String ssid);
+
+    List<WifiData> findByLevelGreaterThan(int level);
+
+    List<WifiData> findByXAndY(float x, float y);
+
+    List<WifiData> findBySsidContaining(String substring);
+
+    @Query(value = "SELECT * FROM wifi_data WHERE level > :level", nativeQuery = true)
+    List<WifiData> findCustomByLevel(@Param("level") int level);
+
+    @Query("SELECT w FROM WifiData w WHERE w.level > :level")
+    List<WifiData> findCustomByLevelJPQL(@Param("level") int level);
+}
